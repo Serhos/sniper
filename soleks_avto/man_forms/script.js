a1b142cc29a12a10c3bd766f17e47a673fbf72f2
sergey <serhos@mail.ru>
2019-09-20 18:54:14 +0700

    start

--- soleks_avto/man_forms/script.js
+++ soleks_avto/man_forms/script.js
@@ -2,10 +2,10 @@ window.addEventListener('DOMContentLoaded', function () {
     'use strict';
 
     function make_format_number (n){
-        let s = '' + n;
-        let l = s.length;
+        var s = '' + n;
+        var l = s.length;
         if (l > 3) {
-            let f_s = '';
+            var f_s = '';
             while (l > 3) {
                 f_s = s.substr(l-3, 3) + ' ' + f_s;
                 l = l-3;
@@ -16,15 +16,15 @@ window.addEventListener('DOMContentLoaded', function () {
         return s;
     }
 
-    let range_inputs = document.querySelectorAll('.js__input_for_range');
+    var range_inputs = document.querySelectorAll('.js__input_for_range');
     range_inputs.forEach(function(el) {
         el.onchange = function (e) {
-            let inp_val = parseInt(el.value.replace(/[^\d]+/g, ''));
-            let range_block = document.querySelector('[data-input="' + el.id + '"]');
-            let min = parseInt(range_block.getAttribute('min'));
-            let new_inp_val = min;
-            let max = parseInt(range_block.getAttribute('max'));
-            let step = parseInt(range_block.getAttribute('step'));
+            var inp_val = parseInt(el.value.replace(/[^\d]+/g, ''));
+            var range_block = document.querySelector('[data-input="' + el.id + '"]');
+            var min = parseInt(range_block.getAttribute('min'));
+            var new_inp_val = min;
+            var max = parseInt(range_block.getAttribute('max'));
+            var step = parseInt(range_block.getAttribute('step'));
             if (isNaN(inp_val) || inp_val < min) {
                 new_inp_val = min;
             } else if (inp_val > max) {
@@ -34,7 +34,7 @@ window.addEventListener('DOMContentLoaded', function () {
                     new_inp_val = new_inp_val + step;
                 }
             }
-            let formated_val = make_format_number(new_inp_val);
+            var formated_val = make_format_number(new_inp_val);
             el.value = formated_val;
             range_block.value = '' + new_inp_val;
             document.getElementById(el.id + '_label').innerHTML = formated_val;
