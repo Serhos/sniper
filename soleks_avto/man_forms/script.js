28d1c3f953540a8c8fc307ad906127391d6c9c9e
sergey <serhos@mail.ru>
2019-09-22 07:14:38 +0700

    start

--- soleks_avto/man_forms/script.js
+++ soleks_avto/man_forms/script.js
@@ -2,10 +2,10 @@ window.addEventListener('DOMContentLoaded', function () {
     'use strict';
 
     function make_format_number (n){
-        var s = '' + n;
-        var l = s.length;
+        let s = '' + n;
+        let l = s.length;
         if (l > 3) {
-            var f_s = '';
+            let f_s = '';
             while (l > 3) {
                 f_s = s.substr(l-3, 3) + ' ' + f_s;
                 l = l-3;
@@ -16,16 +16,27 @@ window.addEventListener('DOMContentLoaded', function () {
         return s;
     }
 
-    var range_inputs = document.querySelectorAll('.js__input_for_range');
-    for (var k in range_inputs) {
-        var el = range_inputs[k];
+    function range_label_set_place(inp_id) {
+        let range_block = document.querySelector('[data-input="' + inp_id + '"]');
+        let label_block = document.getElementById(inp_id + '_label');
+        let label_width = label_block.offsetWidth;
+        let min = parseInt(range_block.getAttribute('min'));
+        let max = parseInt(range_block.getAttribute('max'));
+        let val = parseInt(range_block.value);
+        let width = range_block.offsetWidth;
+        let pos = ((width/(max-min))*(val-min)) - (label_width/2);
+        label_block.style.left = pos + 'px';
+    }
+
+    let range_inputs = document.querySelectorAll('.js__input_for_range');
+    range_inputs.forEach(function(el) {
         el.onchange = function (e) {
-            var inp_val = parseInt(el.value.replace(/[^\d]+/g, ''));
-            var range_block = document.querySelector('[data-input="' + el.id + '"]');
-            var min = parseInt(range_block.getAttribute('min'));
-            var new_inp_val = min;
-            var max = parseInt(range_block.getAttribute('max'));
-            var step = parseInt(range_block.getAttribute('step'));
+            let inp_val = parseInt(el.value.replace(/[^\d]+/g, ''));
+            let range_block = document.querySelector('[data-input="' + el.id + '"]');
+            let min = parseInt(range_block.getAttribute('min'));
+            let new_inp_val = min;
+            let max = parseInt(range_block.getAttribute('max'));
+            let step = parseInt(range_block.getAttribute('step'));
             if (isNaN(inp_val) || inp_val < min) {
                 new_inp_val = min;
             } else if (inp_val > max) {
@@ -35,24 +46,23 @@ window.addEventListener('DOMContentLoaded', function () {
                     new_inp_val = new_inp_val + step;
                 }
             }
-            var formated_val = make_format_number(new_inp_val);
+            let formated_val = make_format_number(new_inp_val);
             el.value = formated_val;
             range_block.value = '' + new_inp_val;
             document.getElementById(el.id + '_label').innerHTML = formated_val;
         };
-    }
+    });
 
-    var range_blocks = document.querySelectorAll('.js__range_for_input');
-    for (var k1 in range_blocks) {
-        var el1=range_blocks[k1];
-        el1.oninput = function (e) {
-            var inp_val = parseInt(el1.value);
-            var input_block = document.getElementById(el1.getAttribute('data-input'));
-            var formated_val = make_format_number(inp_val);
+    let range_blocks = document.querySelectorAll('.js__range_for_input');
+    range_blocks.forEach(function(el) {
+        el.oninput = function (e) {
+            let inp_val = parseInt(el.value);
+            let input_block = document.getElementById(el.getAttribute('data-input'));
+            let formated_val = make_format_number(inp_val);
             input_block.value = formated_val;
             document.getElementById(input_block.id + '_label').innerHTML = formated_val;
         };
-    }
+    });
 
 
 
